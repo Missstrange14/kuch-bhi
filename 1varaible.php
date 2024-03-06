9921781141325f182c700bdd7c691f770e527f90
Missstrange14 <subhasadiq1427@gmail.com>
2024-03-06 18:10:09 +0500

    kuch bhi

---
+++ 1varaible.php
@@ -0,0 +1,21 @@
+<?php
+$x = 589;
+var_dump($x);
+$y = "ana";
+echo "<br>";
+var_dump($y);
+$z = 12.3;
+echo "<br>";
+var_dump($z);
+$a = true;
+echo "<br>";
+var_dump($a);
+echo "<br>";
+$b = [12,13,14];
+echo "<br>";
+var_dump($b);
+
+
+
+
+?>
---
+++ c2 if.php
@@ -0,0 +1,17 @@
+ <?php
+$x = 80;
+$y = 20;
+
+// if ($x > $y) {
+
+//     echo "have a goood day";
+// }
+ if ($x<$y) {
+    # code...
+    echo "you are eligible";
+ }
+ else{
+    echo "you are not eligible";
+ }
+
+?>
